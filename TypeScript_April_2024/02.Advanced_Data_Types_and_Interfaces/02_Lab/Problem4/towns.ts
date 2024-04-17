0f303b9ccc30821dae84a5ecb301ae9617f2de44
HRISTINA STOYANOVA <viara68@abv.bg>
2024-04-17 19:54:03 +0300

    Create towns.ts

---
+++ TypeScript_April_2024/02.Advanced_Data_Types_and_Interfaces/02_Lab/Problem4/towns.ts
@@ -0,0 +1,43 @@
+interface Town {
+    town: string,
+    latitude: string,
+    longitude: string,
+}
+
+const createTown = (townName: string, townLatitude: string, townLongitude: string): Town => {
+    return {
+        town: townName,
+        latitude: townLatitude,
+        longitude: townLongitude,
+    }
+}
+
+function townsObj(towns: string[]): void {
+    const townsArray: object[] = [];
+
+    for (const townData of towns) {
+        const [townName, townLatitude, townLongitude] = townData.split(' | ');
+        const townLatitudeAsStr = Number(townLatitude).toFixed(2).toString();
+        const townLongitudeAsStr = Number(townLongitude).toFixed(2).toString();
+
+        const currTown = createTown(townName, townLatitudeAsStr, townLongitudeAsStr);
+        townsArray.push(currTown);
+    }
+
+    townsArray.forEach(town => {
+        console.log(town);
+    })
+}
+
+townsObj(['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625']);
+townsObj(['Plovdiv | 136.45 | 812.575']);
+
+
+
+
+
+
+
+
+
+//['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625']
