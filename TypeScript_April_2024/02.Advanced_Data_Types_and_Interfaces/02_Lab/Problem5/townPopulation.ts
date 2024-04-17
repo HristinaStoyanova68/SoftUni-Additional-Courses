17c93d89dfadc8fb1922adfec41f453b81a449f7
HRISTINA STOYANOVA <viara68@abv.bg>
2024-04-17 20:21:55 +0300

    Create townPopulation.ts

---
+++ TypeScript_April_2024/02.Advanced_Data_Types_and_Interfaces/02_Lab/Problem5/townPopulation.ts
@@ -0,0 +1,34 @@
+function townsPopulation(array: string[]): void {
+  const towns: { [key: string]: number } = {};
+
+  for (const data of array) {
+    const [town, populationAsStr] = data.split(" <-> ");
+    const population = Number(populationAsStr);
+
+    if (towns.hasOwnProperty(town)) {
+      towns[town] += population;
+    } else {
+      towns[town] = population;
+    }
+  }
+
+  for (const town in towns) {
+    console.log(`${town} : ${towns[town]}`);
+  }
+}
+
+townsPopulation([
+  "Sofia <-> 1200000",
+  "Montana <-> 20000",
+  "New York <-> 10000000",
+  "Washington <-> 2345000",
+  "Las Vegas <-> 1000000",
+]);
+
+townsPopulation([
+  "Istanbul <-> 100000",
+  "Honk Kong <-> 2100004",
+  "Jerusalem <-> 2352344",
+  "Mexico City <-> 23401925",
+  "Istanbul <-> 1000",
+]);
