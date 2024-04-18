d5336f865d8446a90d94b226d294fe68263ffd39
HRISTINA STOYANOVA <viara68@abv.bg>
2024-04-18 18:23:50 +0300

    Update car-info.ts

--- TypeScript_April_2024/03.OOP/03_Lab/src/Problem1/car-info.ts
+++ TypeScript_April_2024/03.OOP/03_Lab/src/Problem1/car-info.ts
@@ -2,15 +2,21 @@ class Car {
   brand: string;
   model: string;
   horsePower: number;
+  _year: number;
 
-  constructor(brand: string, model: string, horsePower: number) {
+  constructor(brand: string, model: string, horsePower: number, year: number) {
     this.brand = brand;
     this.model = model;
     this.horsePower = horsePower;
+    this._year = year;
   }
 
   getBrand(): string {
     return this.brand;
+  }
+
+  get year(): number {
+    return this._year;
   }
   setBrand(brand: string): void {
     this.brand = brand;
@@ -34,13 +40,14 @@ class Car {
 }
 
 // const input = 'Chevrolet Impala 390';
-const input = 'Skoda Karoq 150';
-const [brand, model, horsePowerAsStr] = input.split(' ');
+const input = 'Skoda Karoq 150 2020';
+const [brand, model, horsePowerAsStr, yearAsStr] = input.split(' ');
 const horsePower = Number(horsePowerAsStr);
+const year = Number(yearAsStr);
 
-const myCar = new Car(brand, model, horsePower);
+const myCar = new Car(brand, model, horsePower, year);
 
-console.log(`The car is: ${myCar.getBrand()} ${myCar.getModel()} - ${myCar.getHorsePower()} HP.`);
+console.log(`The car is: ${myCar.getBrand()} ${myCar.getModel()} - ${myCar.getHorsePower()} HP, made at ${myCar._year} year.`);
 
 
 
