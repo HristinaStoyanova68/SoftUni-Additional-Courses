39e1bdce2aa772cb32e79995fc419cc9f0d81671
HRISTINA STOYANOVA <viara68@abv.bg>
2024-04-24 00:34:51 +0300

    reordering files

--- TypeScript_April_2024/01.Introduction_to_TypeScript/01_Lab/src/Problem1/problem1.js
+++
@@ -1,8 +0,0 @@
-function rectangleArea(length, width) {
-    const area = length * width;
-    console.log(area);
-    return area;
-}
-rectangleArea(5, 7);
-rectangleArea(6, 8);
-//# sourceMappingURL=problem1.js.map
