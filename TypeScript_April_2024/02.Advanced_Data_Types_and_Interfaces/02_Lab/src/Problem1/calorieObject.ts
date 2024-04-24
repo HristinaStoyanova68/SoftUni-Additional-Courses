9800b44bb95238fc956fc964b8e4e31842170970
HRISTINA STOYANOVA <viara68@abv.bg>
2024-04-24 16:23:33 +0300

    create user's type

--- TypeScript_April_2024/02.Advanced_Data_Types_and_Interfaces/02_Lab/src/Problem1/calorieObject.ts
+++ TypeScript_April_2024/02.Advanced_Data_Types_and_Interfaces/02_Lab/src/Problem1/calorieObject.ts
@@ -13,3 +13,4 @@ const calorieObject = (array: string[]) => {
 
 calorieObject(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']);
 calorieObject(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42']);
+
