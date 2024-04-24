92d74beb81181045632c218f3aafa334abbae9ae
HRISTINA STOYANOVA <viara68@abv.bg>
2024-04-24 15:55:44 +0300

    initialize workshop-typescript with vite

---
+++ TypeScript_April_2024/07.Workshop/workshop-typescript/src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+const root = document.querySelector<HTMLDivElement>('#app');
+
+root!.innerHTML = `
+  <div>
+    <h1>Workshop-TypeScript</h1>
+  </div>
+`
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
